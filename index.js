4b1257cdfff803e9a9260623e801804422bc9627
Ben McClure <ben.mcclure@gmail.com>
2016-06-26 15:37:47 -0500

    Partial refactor and redesign of API. Breaking update that is not stable yet.

--- index.js
+++ index.js
@@ -2,11 +2,47 @@
  * @author bmcclure
  */
 
+var use = require('use-plugin')({
+  module: module,
+  prefix: 'deploytool',
+  builtin: 'lib'
+});
+
+var deploytoolEnvironment = require('./lib/environment');
+
+function usePlugin(pluginName) {
+  var pluginDescription = use(pluginName);
+
+  pluginDescription.init();
+
+  return pluginDescription;
+}
+
+function deploy(environment, commit, callback) {
+  environment = deploytoolEnvironment.init(environment, {
+    type: '',
+    branch: ''
+  });
+
+  environment.validate(['type', 'branch'], function (error) {
+    if (error) {
+      callback(error);
+
+      return;
+    }
+
+    var plugin = usePlugin(environment.config.type);
+
+    require(plugin.requirepath).deploy(environment, commit, callback);
+  });
+}
+
 module.exports = {
+  cmd: require('./lib/cmd'),
   cmdList: require('./lib/cmd-list'),
-  deploy: require('./lib/deploy'),
-  environment: require('./lib/environment'),
-  execute: require('./lib/execute'),
+  deploy: deploy,
+  environment: deploytoolEnvironment,
   firstCommit: require('./lib/first-commit'),
-  previousCommit: require('./lib/previous-commit')
+  previousCommit: require('./lib/previous-commit'),
+  use: usePlugin
 };
